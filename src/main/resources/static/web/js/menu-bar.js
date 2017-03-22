5ab4e43f127979a09d98703148e5a74114e619e9
zhibo wang <zhibo wang>
2017-03-22 20:43:52 +0800

    bugfix

--- src/main/resources/static/web/js/menu-bar.js
+++ src/main/resources/static/web/js/menu-bar.js
@@ -1,12 +1,14 @@
 $(function() {
-	$(window).scroll(function(){
-        if ($(document).scrollTop() <= 80){
-            $("#header").css("background","none");
-            //alert("滚动条已经到达顶部为 80");
-        } else{
-            $("#header").css("background-color","#333");
-        }
-	});
+//	$(window).scroll(function(){
+//        if ($(document).scrollTop() <= 80){
+//            $("#header").css("background","none");
+//            //alert("滚动条已经到达顶部为 80");
+//        } else{
+//            $("#header").css("background-color","#333");
+//        }
+//	});
+
+	 $("#header").css("background-color","#333");
 });
 
 function toLogin(){
