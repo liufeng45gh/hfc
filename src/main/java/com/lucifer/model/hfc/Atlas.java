294053d3397bd7e8088ed6c7ba958b10091624ff
liufx <liufeng45gh@163.com>
2017-05-02 22:10:15 +0800

    atlas

---
+++ src/main/java/com/lucifer/model/hfc/Atlas.java
@@ -0,0 +1,57 @@
+package com.lucifer.model.hfc;
+
+/**
+ * Created by fx on 2017/5/2.
+ */
+public class Atlas {
+
+    private Long id;
+
+    private String logo;
+
+    private String title;
+
+    private Float top;
+
+    private Integer isDeleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getTop() {
+        return top;
+    }
+
+    public void setTop(Float top) {
+        this.top = top;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
---
+++ src/main/java/com/lucifer/model/hfc/Picture.java
@@ -0,0 +1,67 @@
+package com.lucifer.model.hfc;
+
+/**
+ * Created by fx on 2017/5/2.
+ */
+public class Picture {
+
+    private Long id;
+
+    private String url;
+
+    private String description;
+
+    private Long atlasId;
+
+    private Float top;
+
+    private Integer isDeleted;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getAtlasId() {
+        return atlasId;
+    }
+
+    public void setAtlasId(Long atlasId) {
+        this.atlasId = atlasId;
+    }
+
+    public Float getTop() {
+        return top;
+    }
+
+    public void setTop(Float top) {
+        this.top = top;
+    }
+
+    public Integer getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Integer isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+}
