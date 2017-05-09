cade4d8f8a16a9ce62d25119347ccdc83982b884
刘峰选 <liufengxuan@9hcom.com>
2017-05-09 17:14:20 +0800

    test

---
+++ src/main/java/com/lucifer/model/hfc/Post.java
@@ -0,0 +1,69 @@
+package com.lucifer.model.hfc;
+
+import java.util.Date;
+
+/**
+ * Created by liufx on 2017/5/9.
+ */
+public class Post {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String contacts;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(String contacts) {
+        this.contacts = contacts;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
